89e0fa5085d04b4feaf483192ca897919221d35e
D1S01 <diegosg513@gmail.com>
2025-11-10 01:41:16 -0300

    feature: login con crispy_forms y links estilizados

--- registration/forms.py
+++ registration/forms.py
@@ -1,4 +1,10 @@
+# para el css del fromulario
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field, Submit
+# <========================== para heredar  ========================>
+from django.contrib.auth.forms import AuthenticationForm
+# <==========================  ========================>
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
@@ -9,3 +15,15 @@ class CustomUserCreationForm(UserCreationForm):
     class Meta:
         model = User
         fields = ("username", "email", "password1", "password2")
+
+class LoginForm(AuthenticationForm):
+    def __init__(self, *args, **kwargs):
+        # super(LoginForm, self).__init__(*args, **kwargs)
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'post'
+        self.helper.layout = Layout(
+            Field('username', css_class='input-field'),
+            Field('password', css_class='input-field'),
+            Submit('submit', 'Ingresar', css_class='btn btn-green')
+        )
--- registration/views.py
+++ registration/views.py
@@ -2,18 +2,18 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib import messages
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, LoginForm
 from usuarios.models import Usuario
 
 # Create your views here.
 def login_view(request):
     if request.method == "POST":
-        form = AuthenticationForm(request, data=request.POST)
+        form = LoginForm(request, data=request.POST)
         if form.is_valid():
             user = form.get_user()
             login(request, user)
             return redirect('producto-list')
-    form = AuthenticationForm()
+    form = LoginForm()
     return render(request, 'registration/login.html', {'form':form})
 
 def registrar(request):
